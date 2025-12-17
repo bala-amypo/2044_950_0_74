6ca310dadb0e7b94abd383108989405ca328ac8d
bala-amypo <bala@amypo.in>
2025-12-17 06:39:33 +0000

    User commit

--- src/main/java/com/example/demo/Repositary/StudentsRepo.java
+++ src/main/java/com/example/demo/Repositary/StudentsRepo.java
@@ -1,4 +1,5 @@
 package com.example.demo.Repositary;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.jpa.Repositary;
 import org.springframework.stereotype.Repositary;
