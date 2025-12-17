6f72ebc18d34663188708c47b9fb078e18443c99
bala-amypo <bala@amypo.in>
2025-12-17 08:06:50 +0000

    User commit

--- src/main/java/com/example/demo/Repositary/StudentsRepo.java
+++ src/main/java/com/example/demo/Repositary/StudentsRepo.java
@@ -3,6 +3,7 @@ package com.example.demo.Repositary;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.jpa.Repositary;
 import org.springframework.stereotype.Repositary;
+import com.example.sql.Entitydata.Student;
 
 @Repositary
 public interface StudentsRepositary extends jpaRepositary<StudentEntity,Integer>{
