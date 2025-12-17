69417fb8fcaf614f212f4d2ac4d84f8d647a760e
bala-amypo <bala@amypo.in>
2025-12-17 04:14:33 +0000

    User commit

--- src/main/java/com/example/demo/Controller/basics.java
+++ src/main/java/com/example/demo/Controller/basics.java
@@ -1,6 +1,6 @@
 package com.example.demo.Controller;
 import org.springframework.web.bind.annotation.*;
-@restController
+@RestController
 public calss basics {
     @GetMapping("/home")
     public String name() {
